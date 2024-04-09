450f91420d63d5b59ce4f4c2750d4e6582e48efa
William Burleson <72857839+Admiral-Billy@users.noreply.github.com>
2024-04-08 21:32:34 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pokerogue App
  An app to play Pokerogue.net in an app window. Wow!
+
+Build instructions:
+
+Install NodeJS (https://nodejs.org/en)
+
+Open a command prompt to the directory you stick this repo in
+
+Install electron (npm install electron --save-dev) and the electron packager (npm install electron-packager --save-dev)
+
+Run from command prompt with "npm start" or build the full app with "npm run package".
+
+Play! The app is found in dist/Pokerogue/Pokerogue.exe if you built it that way.
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 749, // 1 smaller to force the game to resize and fix a weird startup resolution issue
+    autoHideMenuBar: true,
+	icon: 'icons/PR',
+    webPreferences: {
+      nodeIntegration: false,
+      persistSessionStorage: true,
+      persistUserDataDirName: 'Pokerogue'
+    }
+  });
+
+   win.loadURL('https://pokerogue.net/');
+   win.webContents.on('did-finish-load', () => {
+   const gameWidth = 1280;
+   const gameHeight = 750;
+   win.setSize(gameWidth, gameHeight);
+   win.center();
+ });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
