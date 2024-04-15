e59e9a42d1807805562bdda4f87985ed0d0e504a
William Burleson <72857839+Admiral-Billy@users.noreply.github.com>
2024-04-14 20:12:17 -0400

    Implemented loading screen + auto-retry on failed load

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { spawn } = require('child_process');
 const fs = require('fs');
@@ -6,6 +6,7 @@ const treeKill = require('tree-kill');
 
 let viteProcess;
 let mainWindow;
+let loadingWindow;
 
 async function createWindow() {
   mainWindow = new BrowserWindow({
@@ -13,6 +14,7 @@ async function createWindow() {
     height: 749,
     autoHideMenuBar: true,
     icon: 'icons/PR',
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       persistSessionStorage: true,
@@ -20,14 +22,35 @@ async function createWindow() {
     }
   });
 
+  loadingWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    autoHideMenuBar: true,
+    resizable: false,
+    alwaysOnTop: true,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  loadingWindow.loadFile('loading.html');
+
   const readyToStart = await startServer();
 
   mainWindow.loadURL('http://localhost:8000');
+  mainWindow.webContents.on('did-fail-load', () => {
+    setTimeout(() => {
+      mainWindow.loadURL('http://localhost:8000');
+    }, 1000);
+  });
   mainWindow.webContents.on('did-finish-load', () => {
     const gameWidth = 1280;
     const gameHeight = 750;
     mainWindow.setSize(gameWidth, gameHeight);
     mainWindow.center();
+    mainWindow.show();
+    loadingWindow.close();
   });
 
   mainWindow.on('close', (event) => {
@@ -56,7 +79,7 @@ function startServer() {
     shell: true,
     stdio: 'ignore'
   });
-  
+
   return Promise.resolve(1);
 }
 
@@ -77,6 +100,12 @@ function showErrorBox() {
     app.quit();
   });
 }
+
+ipcMain.on('close-loading-screen', () => {
+  if (loadingWindow) {
+    loadingWindow.close();
+  }
+});
 
 app.whenReady().then(() => {
   createWindow();
