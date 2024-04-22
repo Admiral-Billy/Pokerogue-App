8eff16d1476bc6dfd99ac8d36275441336e0a946
William Burleson <72857839+Admiral-Billy@users.noreply.github.com>
2024-04-22 06:49:28 -0400

    Attempted copy/paste support for mac

--- main.js
+++ main.js
@@ -157,7 +157,18 @@ async function createWindow() {
 			  }
 			}
 		  ]
-		}
+		},
+		{
+		label: "Edit",
+		submenu: [
+			{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			{ type: "separator" },
+			{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]}
 	  ];
   }
   else {
@@ -214,7 +225,18 @@ async function createWindow() {
 			  }
 			}
 		  ]
-		}
+		},
+		{
+		label: "Edit",
+		submenu: [
+			{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			{ type: "separator" },
+			{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]}
 	  ];
   }
 
